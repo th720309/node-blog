ebcae98f4b89c35d3c366c5e604249fe334d30a4
th720309 <646595546@qq.com>
2017-05-03 12:30:39 +0800

    express learning

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Nicholas on 2017/5/2.
+ */
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+    // 404 page
+    app.use(function (req, res) {
+        if (!res.headersSent) {
+            res.status(404).render('404');
+        }
+    });
+};
